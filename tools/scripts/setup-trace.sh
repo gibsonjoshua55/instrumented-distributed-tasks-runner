cd2deff1639472461240d32eeba53a7ab227de08
Josh Gibson <gibsonjoshua55@gmail.com>
2021-10-23 14:29:22 -0400

    fix(action): fix install command

--- tools/scripts/setup-trace.sh
+++ tools/scripts/setup-trace.sh
@@ -1,6 +1,6 @@
 # install otel cli
 wget --no-check-certificate https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli_0.0.18_x86_64.deb
-apt install ./otel-cli_0.0.18_x86_64.deb
+sudo apt install -y ./otel-cli_0.0.18_x86_64.deb
 
 # create the trace in honeycomb
 otel-cli span \
