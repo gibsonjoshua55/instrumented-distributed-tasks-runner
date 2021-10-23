6f0322bae75699fea489e0978cdad5ac133ac37f
Josh Gibson <gibsonjoshua55@gmail.com>
2021-10-23 14:44:11 -0400

    fix(action): create span for driver commands

---
+++ tools/scripts/install-otel-cli.sh
@@ -0,0 +1,3 @@
+# install otel cli
+wget --no-check-certificate https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli_0.0.18_x86_64.deb
+sudo apt install -y ./otel-cli_0.0.18_x86_64.deb
--- tools/scripts/setup-trace.sh
+++ tools/scripts/setup-trace.sh
@@ -1,6 +1,4 @@
-# install otel cli
-wget --no-check-certificate https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli_0.0.18_x86_64.deb
-sudo apt install -y ./otel-cli_0.0.18_x86_64.deb
+sh tools/scripts/install-otel-cli.sh
 
 # create the trace in honeycomb
 echo "Creating a span"
