8cbac513c4a0a31703c1f5bff60a97d8c52477d0
Josh Gibson <gibsonjoshua55@gmail.com>
2021-10-23 14:27:32 -0400

    fix deps

--- tools/scripts/setup-trace.sh
+++ tools/scripts/setup-trace.sh
@@ -1,12 +1,13 @@
 # install otel cli
-go get github.com/equinix-labs/otel-cli
+wget --no-check-certificate https://github.com/equinix-labs/otel-cli/releases/download/v0.0.18/otel-cli_0.0.18_x86_64.deb
+apt install ./otel-cli_0.0.18_x86_64.deb
 
 # create the trace in honeycomb
 otel-cli span \
   -n root-task
   -s nx-cli \
-  --otlp-headers x-honeycomb-team=$HONEYCOMB_WRITEKEY,x-honeycomb-dataset=$HONEYCOMB_DATASET
-  --endpoint api.honeycomb.io:443
+  --otlp-headers x-honeycomb-team=$HONEYCOMB_WRITEKEY,x-honeycomb-dataset=$HONEYCOMB_DATASET \
+  --endpoint api.honeycomb.io:443 \
   --tp-print \
   --tp-export > tools/scripts/set-trace-env.sh
 
