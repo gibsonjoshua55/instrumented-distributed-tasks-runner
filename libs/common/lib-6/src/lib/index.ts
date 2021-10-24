c765ecf942ec9db90c0ccdd758247e7eb29ada9f
Josh Gibson <gibsonjoshua55@gmail.com>
2021-10-24 12:15:23 -0400

    fix(distributed-build): parallelize, no tests, always end agents

--- libs/common/lib-6/src/lib/index.ts
+++ libs/common/lib-6/src/lib/index.ts
@@ -1,5 +1,5 @@
+export * from './lib';
 export * as fns from './functions';
 export * as helpers from './helpers';
 export * as utilities from './utilities';
 export * as utils from './utils';
-export * from './lib';
